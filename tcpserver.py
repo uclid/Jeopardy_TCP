a96e9657c59f6d6a7334c4fc28a059354ecd161f
Dixit Bhatta <uclid_dixit@yahoo.com>
2018-05-15 15:13:09 -0400

    Implemented the broadcast
    Successfully broadcast the same message to all clients. Now, waiting for updates on client side to make them wait until category is selected by the designated player.

--- tcpserver.py
+++ tcpserver.py
@@ -9,6 +9,7 @@ bind_port = 9999
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
 server.listen(5)  # max backlog of connections
+client_list = []
 
 MIN_PLAYERS = 3
 
@@ -40,45 +41,41 @@ def handle_client_connection(client_socket):
     client_message = json.loads(request.decode())
     print ('Received {}'.format(client_message['name']))
     
-    player_names.add(client_message['name'])
+    player_names.append(client_message['name'])
     num_players = threading.activeCount() -1
       
-    #game in progress
-    while num_players < MIN_PLAYERS:
-        #keep checking active connections
-        num_players = threading.activeCount() -1
+    #game in progress 
+    while len(player_names) < MIN_PLAYERS:
+        #keep waiting until all have input their names
+        2==2
     
-    if num_players >= MIN_PLAYERS:
-        SM_NEW_GAME = { 'num_players' : num_players,
-                    'player_names' : player_names,
-                    'num categories' : num_categories,
-                    'categories' : categories,
-                    'ques_in_categories' : ques_in_categories,
-                    'def_timeout' : def_timeout }
+    SM_NEW_GAME = { 'num_players' : num_players,
+                'player_names' : player_names,
+                'num categories' : num_categories,
+                'categories' : categories,
+                'ques_in_categories' : ques_in_categories,
+                'def_timeout' : def_timeout }
     
     message = json.dumps(SM_NEW_GAME)
     client_socket.send(message.encode())
     
     #round in progress
-    selected_player = random.randint(1,num_players+1)
-    SM_NEW_ROUND = {"selected_player" : selected_player}
+    broadcast_round(client_socket, num_players)
     
-    message = json.dumps(SM_NEW_ROUND)
-    client_socket.send(message.encode())
-    
-    #category_selected
+    #category_selected (wait until a category is picked by the selected player)
     request = client_socket.recv(1024)
     client_message = json.loads(request.decode())
     print ('Received from player {} the category {}'.format(client_message['player_id'],client_message['category_id'])) #getting category from client 
-    category = client_message['category_id'])
+    category = client_message['category_id']
     
     selected_question = random.randint(1,2)
     SM_QUESTION = {"selected_question" : questions[category][selected_question]}
     
     message = json.dumps(SM_QUESTION)
-    client_socket.send(message.encode())    
+    client_socket.send(message.encode())  
     
     #waiting for ring
+    ring = False
     
     
     
@@ -90,12 +87,27 @@ def handle_client_connection(client_socket):
     client_socket.send(b'END')
     client_socket.close()
 
+def broadcast_round(client_socket, num_players):
+    selected_player = random.randint(1,num_players+1)
+    SM_NEW_ROUND = {"selected_player" : selected_player}
+    
+    message = json.dumps(SM_NEW_ROUND)
+    
+    for clients in client_list:
+        clients.send(message.encode())    
+
+def remove(connection):
+    if connection in client_list:
+        client_list.remove(connection)
+
 while True:
-    client_sock, address = server.accept()
-    print ('Accepted connection from {}:{}'.format(address[0], address[1]))
-    client_handler = threading.Thread(
-        target=handle_client_connection,
-        args=(client_sock,)  # without comma you'd get a... TypeError: handle_client_connection() argument after * must be a sequence, not _socketobject
-    )
-    print ('Number of active clients: {}'.format(threading.activeCount()))
-    client_handler.start()
+    if((threading.activeCount() - 1) < MIN_PLAYERS):
+        client_sock, address = server.accept()
+        client_list.append(client_sock)
+        print ('Accepted connection from {}:{}'.format(address[0], address[1]))
+        client_handler = threading.Thread(
+            target=handle_client_connection,
+            args=(client_sock,)  # without comma you'd get a... TypeError: handle_client_connection() argument after * must be a sequence, not _socketobject
+        )
+        print ('Number of active clients: {}'.format(threading.activeCount()))
+        client_handler.start()

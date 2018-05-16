f121c09f9d03d10b75b15f3e65e0f3d268118e6e
AsifRabi <bit0432@iit.du.ac.bd>
2018-05-16 01:56:29 -0400

    Update client_new.py

--- client_new.py
+++ client_new.py
@@ -11,11 +11,17 @@ connected = False
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost',8888))
 connected = True
+name = input('Your name: ')
+CM_SUBSCRIBE = {'name': name}
+message = json.dumps(CM_SUBSCRIBE)
+client_socket.send(message.encode())
 
 while connected == True:
     #wait for server commands to do things, now we will just display things
-    data = client_socket.recv(1024)     
-    cmd = json.loads(data) #we now only expect json   
+    data = client_socket.recv(1024) 
+    #decoded = data.decode()
+    #list_message = decoded.split('\n')
+    cmd = json.loads(data.decode()) #we now only expect json
     print(cmd)
     '''if(cmd['type'] == 'bet'):
         bet = cmd['value']
